57fcdbd5a7417ba1226ce72ab0c0a0dba3107f7c
omerfaru <34684709+omerfaru@users.noreply.github.com>
2019-05-16 14:33:49 +0530

    updated
    updated

--- skills/variable.js
+++ skills/variable.js
@@ -5,6 +5,7 @@ module.exports = function (controller) {
 
     controller.hears([/^variables$/], 'direct_message,direct_mention', function (bot, message) {
         var text = "Variables are nothing but reserved memory locations to store values. It means that when you create a variable, you reserve some space in the memory.Based on the data type of a variable, the interpreter allocates memory and decides what can be stored in the reserved memory. Therefore, by assigning different data types to the variables, you can store integers, decimals or characters in these variables.";
+        bot.reply(message, text);
 
 
     });
