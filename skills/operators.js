448e3a109bb3123b746a3b267cff2a28bdf36d95
omerfaru <34684709+omerfaru@users.noreply.github.com>
2019-05-16 15:12:44 +0530

    updated
    updated

--- skills/operators.js
+++ skills/operators.js
@@ -4,7 +4,7 @@
 module.exports = function (controller) {
 
     controller.hears([/^operators$/], 'direct_message,direct_mention', function (bot, message) {
-        var text = "Operators are used to perform operations on variables and values.Python divides the operators in the following:Arithmetic operators,Assignment operators,Comparison operators,Logical operators,Identity operators,Membership operators,Bitwise operators.;
+        var text = "Operators are used to perform operations on variables and values.Python divides the operators in the following:Arithmetic operators,Assignment operators,Comparison operators,Logical operators,Identity operators,Membership operators,Bitwise operators.";
         bot.reply(message, text);
     });
 }
