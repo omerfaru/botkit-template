8217d8940f5826fc87ee22d0c98a2a647cab9cc2
omerfaru <34684709+omerfaru@users.noreply.github.com>
2019-05-16 14:27:30 +0530

    updated
    updated

--- skills/variable.js
+++ skills/variable.js
@@ -7,6 +7,7 @@ module.exports = function (controller) {
 
         bot.startConversation(message, function (err, convo) {
             convo.say('Variables are nothing but reserved memory locations to store values. It means that when you create a variable, you reserve some space in the memory.Based on the data type of a variable, the interpreter allocates memory and decides what can be stored in the reserved memory. Therefore, by assigning different data types to the variables, you can store integers, decimals or characters in these variables.');
+            });
 
         });
 
