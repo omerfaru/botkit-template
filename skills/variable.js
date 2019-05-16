25c4f4e0bf95f9217148526b83db03f5fb63154d
omerfaru <34684709+omerfaru@users.noreply.github.com>
2019-05-16 15:00:07 +0530

    updated
    updated

--- skills/variable.js
+++ skills/variable.js
@@ -1,9 +1,9 @@
 //
-// Command: pyvariable
+// Command: variable
 //
 module.exports = function (controller) {
 
-    controller.hears([/^pyvariable$/], 'direct_message,direct_mention', function (bot, message) {
+    controller.hears([/^variable$/], 'direct_message,direct_mention', function (bot, message) {
         var text = "In Python, variables are a storage placeholder for texts and numbers. It must have a name so that you are able to find it again. The variable is always assigned with the equal sign, followed by the value of the variable.";
         bot.reply(message, text);
     });
