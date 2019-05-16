ef38975dc4104d440ed14b8b14a949c66487e535
omerfaru <34684709+omerfaru@users.noreply.github.com>
2019-05-16 14:52:27 +0530

    updated
    updated

--- skills/pyvariable.js
+++ skills/pyvariable.js
@@ -4,7 +4,7 @@
 module.exports = function (controller) {
 
     controller.hears([/^pyvariable$/], 'direct_message,direct_mention', function (bot, message) {
-        var text = "Variables are nothing but reserved memory locations to store values. It means that when you create a variable, you reserve some space in the memory.Based on the data type of a variable, the interpreter allocates memory and decides what can be stored in the reserved memory. Therefore, by assigning different data types to the variables, you can store integers, decimals or characters in these variables.";
+        var text = "Variables are nothing but reserved memory locations to store values.";
         bot.reply(message, text);
     });
 }
